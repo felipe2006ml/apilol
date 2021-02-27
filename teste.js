40bea8faecb70115065f43832189d1a23c8dc9c3
Luis Lopes <felipe_2006ml@hotmail.com>
2021-02-27 01:07:19 -0300

    teste

---
+++ teste.js
@@ -0,0 +1,50 @@
+const contentT = document.getElementById('content2')
+const inputLevel = document.getElementById('input-level')
+
+async function getChamp(nome) {
+
+    let url = `http://ddragon.leagueoflegends.com/cdn/11.4.1/data/pt_BR/champion/${nome}.json`
+
+
+    const response = await fetch(url)
+    const dadosJSON = await response.json()
+
+    let dados = dadosJSON.data
+
+    showChamp(dados[nome])
+}
+
+function showChamp(statsL) {
+    
+    let sLevel = statsL
+    let valorInput = inputLevel.value
+
+    let hp = sLevel.stats.hp
+    let movespeed = sLevel.stats.movespeed
+    let attackrange = sLevel.stats.attackrange
+    let attackspeed = sLevel.stats.attackspeed
+    let attackdamage = sLevel.stats.attackdamage
+    let armor = sLevel.stats.armor
+    let spellblock = sLevel.stats.spellblock
+    let mp = sLevel.stats.mp
+
+    let hppl = sLevel.stats.hpperlevel
+    let attackspeedpl = sLevel.stats.attackspeedperlevel
+    let attackdamagepl = sLevel.stats.attackdamageperlevel
+    let armorpl = sLevel.stats.armorperlevel
+    let spellblockpl = sLevel.stats.spellblockperlevel
+    let mppl = sLevel.stats.mpperlevel
+
+    console.log(hp+(valorInput * hppl))
+    
+    contentT.innerHTML =
+    `<p>Vida base: ${hp+(valorInput * hppl)}</p>
+    <p>Velocidade de Movimento base: ${movespeed}</p>
+    <p>Distancia de Ataque: ${attackrange}</p>
+    <p>Velocidade de Ataque Base: ${attackspeed}</p>
+    <p>Dano de Ataque Base: ${attackdamage}</p>
+    <p>Armadura Base: ${armor}</p>
+    <p>Resistência Mágica: ${spellblock}</p>
+    <p>Mana Base: ${mp}</p>
+    `
+}
